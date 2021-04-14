49d60217e717d5d4283337c58b204c6cc6839235
kolatez <82465431+kolatez@users.noreply.github.com>
2021-04-13 20:31:16 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+const fs = require('fs');
 const Discord = require('discord.js');
 const config = require('./config.json');
-const client = new Discord.Client();
 
-fs = require('fs')
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+
+	client.commands.set(command.name, command);
+}
+
 var data;
 fs.readFile('insults.txt', 'utf8', function (err,rawData) {
   if (err) {
@@ -20,24 +30,29 @@ function getRandomLine(){
 }
 
 prefix = "<"
-taggedUser = "@alphaMimicBot"
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
 client.on('message', message => {
-	if (message.content === `${prefix}ping`) {
-        // sends back ur MOM
-        message.channel.send('shut the fuck up im not mee6 dumbass');
-}   else if (message.content === `${prefix}beep`) {
-	message.channel.send('BOOP IM A BOT MOTHER FUCKAAAAAAA');
+	  if (message.content === `${prefix}ping`) {
+        client.commands.get('ping').execute(message);
+}   
+    else if (message.content === `${prefix}beep`) {
+	  message.channel.send('BOOP IM A BOT MOTHER FUCKAAAAAAA');
     
 }
     else if (message.content.startsWith(`<@!762725950361305128>`)) {
-    message.channel.send(getRandomLine());
+      message.channel.send(getRandomLine());
+    
+}   
+    else if (message.content.startsWith(`${prefix}what is`)) {
+      client.commands.get('whatis').execute(message);
 }
 
 });
 
 client.login(config.token);
+
+// credits to my bois over in the noobs discord server ayyy
